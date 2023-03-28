bdb7ad956f3cb36084c39811d5bf96294a2114c0
justinejay01 <66052057+justinejay01@users.noreply.github.com>
2023-03-28 15:07:21 +0800

    update 28/03/23

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const path = require('path');
 const router = express.Router();
 
 const app = express();
+
 const port = process.env.port || 3000;
+
+app.use("public", express.static(__dirname + '/public'));
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
 app.use('/', router);
-app.use('/static', express.static(path.join(__dirname + 'static')));
+
 app.listen(port, () => {
     console.log('Running on port ' + port);
 });
