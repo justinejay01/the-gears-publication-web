7e97b828d2fc7523649f4c6f506699db299181ad
justinejay01 <66052057+justinejay01@users.noreply.github.com>
2023-05-29 15:42:06 +0800

    init news

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const router = express.Router();
+const mysql = require("mysql");
 
 const app = express();
 
 const port = process.env.port || 3000;
 
-app.use("/assets", express.static(path.join(__dirname, 'public')));
-
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/index.html'));
+var con = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "the_gears_publication",
 });
 
-router.get('/sample', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/sample.html'));
-})
+app.use("/assets", express.static(path.join(__dirname, "public")));
 
-app.use('/', router);
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/index.html"));
+});
+
+router.get("/sample", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/index1.html"));
+});
+
+router.get("/news", (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/news.html"));
+});
+
+router.get("/get_news", (req, res) => {
+  con.getConnection((err, connection) => {
+    if (err) throw err;
+    connection.query("SELECT news_title, news_desc FROM news_articles;", (error, results, fields) => {
+        var jsonResults = JSON.stringify(results);
+        res.send(jsonResults);
+        connection.release();
+        if (error) throw error;
+      }
+    );
+  });
+});
+
+app.use("/", router);
 
 app.listen(port, () => {
-    console.log('Running on port ' + port);
+  console.log("Running on port " + port);
 });
