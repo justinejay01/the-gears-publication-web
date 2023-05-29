27572de384f3a2b77d6bb520a4c2ec7490993d28
justinejay01 <66052057+justinejay01@users.noreply.github.com>
2023-05-29 23:10:09 +0800

    update get news

--- server.js
+++ server.js
@@ -20,18 +20,18 @@ router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
-router.get("/sample", (req, res) => {
-  res.sendFile(path.join(__dirname + "/views/index1.html"));
-});
+router.get("/news/:id?", (req, res) => {
+  const id = req.params.id;
 
-router.get("/news", (req, res) => {
-  res.sendFile(path.join(__dirname + "/views/news.html"));
+  if (id === undefined) res.sendFile(path.join(__dirname + "/views/news.html"));
+  else res.sendFile(path.join(__dirname + "/views/news_details.html"));
+  
 });
 
 router.get("/get_news", (req, res) => {
   con.getConnection((err, connection) => {
     if (err) throw err;
-    connection.query("SELECT news_title, news_desc FROM news_articles;", (error, results, fields) => {
+    connection.query("SELECT news_id, news_title FROM news_articles;", (error, results, fields) => {
         var jsonResults = JSON.stringify(results);
         res.send(jsonResults);
         connection.release();
